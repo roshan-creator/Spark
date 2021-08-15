a6ed724797030684b75522dfd8caf83dda1d5097
roshan-creator <roshanpatidar161@gmail.com>
2021-08-15 20:14:47 +0530

    Spark Internship Project

---
+++ footer.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+footer {
+    background-color:#32a0a8;
+    text-align: center;
+    color: white;
+    padding: 20px;
+}
+.sticky-footer {
+    position: sticky;
+    top: 100%;
+    margin-top: 5%;
+}
+</style>
+</head>
+<body>
+    
+<footer class="sticky-footer">
+            <p>&copy 2021. Made by <b>Roshan Patidar</b> <br></p>
+</footer>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #b78b48;"><b>SWEEP BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="first-page.php" style="color :#054e0e;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #054e0e;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #054e0e;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #054e0e;"><b>Transaction History</b></a>
+              </li>
+            </ul>
+        </div>
+</nav>
